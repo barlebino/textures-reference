c7ff0fcb40680a4a99df56b1a70ec9bb1bc37e89
Brenan <barlebino@tutanota.com>
2017-09-25 17:24:38 -0700

    Added image loading code

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,16 @@
 
 #include <unistd.h>
 
+// Image code, for textures
+struct Image {
+  int sizeX, sizeY;
+  char *data;
+};
+
+struct RGB {
+  GLubyte r, g, b;
+};
+
 GLFWwindow *window; // Main application window
 
 // Location of camera
@@ -43,8 +53,98 @@ GLint placementLoc;
 
 int g_width, g_height;
 
-// Testing
-bool first = true;
+// Helper functions for image load
+static unsigned int getint(FILE *fp) {
+  int c, c1, c2, c3;
+	
+  // Get 4 bytes
+  c = getc(fp);
+  c1 = getc(fp);
+  c2 = getc(fp);
+  c3 = getc(fp);
+	
+	return ((unsigned int) c) + (((unsigned int) c1) << 8) +
+	(((unsigned int) c2) << 16) + (((unsigned int) c3) << 24);
+}
+
+static unsigned int getshort(FILE *fp) {
+	int c, c1;
+	
+	// Get 2 bytes
+	c = getc(fp);
+	c1 = getc(fp);
+	
+	return ((unsigned int) c) + (((unsigned int) c1) << 8);
+}
+
+int ImageLoad(const char *filename, Image *image) {
+	FILE *file;
+	unsigned long size; // Size of the image in bytes
+	unsigned long i; // Standard counter
+	unsigned short int planes; // Number of planes in image (must be 1)
+	unsigned short int bpp; // Number of bits per pixel (must be 24)
+	char temp; // Used to convert bgr to rgb color
+	
+	// Make sure the file is there
+	if((file = fopen(filename, "rb")) == NULL) {
+		printf("File Not Found : %s\n", filename);
+		return 0;
+	}
+	
+	// Seek through the bmp header, up to the width height:
+	fseek(file, 18, SEEK_CUR);
+	
+	// No 100% errorchecking anymore!!!
+	
+	// Read the width
+  image->sizeX = getint(file);
+	
+	// Read the height
+	image->sizeY = getint(file);
+	
+	// Calculate the size (assuming 24 bits or 3 bytes per pixel)
+	size = image->sizeX * image->sizeY * 3;
+	
+	// Read the planes
+	planes = getshort(file);
+	if(planes != 1) {
+		printf("Planes from %s is not 1: %u\n", filename, planes);
+		return 0;
+	}
+	
+	// Read the bpp
+	bpp = getshort(file);
+	if(bpp != 24) {
+		printf("Bpp from %s is not 24: %u\n", filename, bpp);
+		return 0;
+	}
+	
+	// Seek past the rest of the bitmap header
+	fseek(file, 24, SEEK_CUR);
+	
+	// Read the data
+	image->data = (char *) malloc(size);
+	if(image->data == NULL) {
+		printf("Error allocating memory for color-corrected image data");
+		return 0;
+	}
+	
+	if((i = fread(image->data, size, 1, file)) != 1) {
+		printf("Error reading image data from %s.\n", filename);
+		return 0;
+	}
+	
+	for(i = 0; i < size; i += 3) { // Reverse all of the colors (bgr -> rgb)
+		temp = image->data[i];
+		image->data[i] = image->data[i+2];
+		image->data[i+2] = temp;
+	}
+	
+	fclose(file); // Close the file and release the filedes
+	
+	// We're done
+	return 1;
+}
 
 // For debugging
 void printMatrix(glm::mat4 mat) {
@@ -202,7 +302,8 @@ static void init() {
   glEnable(GL_BLEND);
 
   // Get mesh
-  getMesh("../resources/bunny.obj");
+  // getMesh("../resources/bunny.obj");
+  getMesh("../resources/sphere.obj");
   resizeMesh(posBuf);
 
   // Send mesh to GPU
@@ -322,9 +423,7 @@ static void render() {
     glm::value_ptr(matPlacement));
 
   // Bind vertex array object
-  if(first) {
-    glBindVertexArray(vaoID);
-  }
+  glBindVertexArray(vaoID);
 
   // Draw
   glDrawElements(GL_TRIANGLES, (int) eleBuf.size(), GL_UNSIGNED_INT,
@@ -387,12 +486,6 @@ int main(int argc, char **argv) {
     render();
     // Swap front and back buffers
     glfwSwapBuffers(window);
-
-    // Testing
-    if(first) {
-      sleep(3);
-      first = false;
-    }
 
     // Poll for and process events
     glfwPollEvents();
